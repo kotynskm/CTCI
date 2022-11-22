c4bc40227676ebdd9792f57fba453cd529800182
Kailey <k.kotynski@gmail.com>
2022-11-22 12:05:35 -0800

    Added solution for deletemiddlenode.py

--- deletemiddlenode.py
+++ deletemiddlenode.py
@@ -29,3 +29,24 @@ class Solution:
         prev.next = prev.next.next
 
         return head
+
+    # solution using only fast and slow - set fast ahead by 2 initially
+    # Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head.next:
+            return None
+
+        slow, fast = head, head.next.next
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        slow.next = slow.next.next
+
+        return head
